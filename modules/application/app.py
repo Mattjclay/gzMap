74f92745d6f1cd476a8e08269130a0b59158f1ca
Matthew Clay <mattjclay@outlook.com>
2023-04-06 19:15:20 -0400

    added some basic interactions

--- modules/application/app.py
+++ modules/application/app.py
@@ -8,22 +8,63 @@ from printy import printy
 app_name =  pyfiglet.figlet_format("gzmap", font = "doh", width = 200)
 welcome_mesage = "Welcome to gzmap, Use the Arrow Keys to navigate and press Enter to select : "
 
-# Colors
+# Colors for printy
 yellow = "y"
 magenta = "m"
 
+# Application Entry Point
 def run():
     term.clear()
     printy(app_name, yellow)
     printy(welcome_mesage, magenta)
+    
+    menu_main()
+    
+    
+def menu_main():
     questions = [
         inquirer.List(
             "action",
             message="What would you like to do?",
-        choices=["nmap", "help", "exit"],
+        choices=["Nmap", "Help", "Exit"],
         ),
     ]
+    action = inquirer.prompt(questions)
+    
+    match (action.get("action")):
+        case "Nmap":
+            printy("Nmap Selected", yellow)
+            menu_nmap()
+        case "Help":
+            printy("Help Selected", yellow)
+        case "Exit":
+            printy("Exiting", yellow)
+            subprocess.run("clear")
+            exit()
+        case _: 
+            printy("Invalid Option", yellow)
+            run()
 
-    answers = inquirer.prompt(questions)
-    printy(answers, magenta)
-
+def menu_nmap():
+    questions = [
+        inquirer.List(
+            "action",
+            message="What would you like to do?",
+        choices=["Banners", "Port Scan", "Exit"],
+        ),
+    ]
+    action = inquirer.prompt(questions)
+    
+    match (action.get("action")):
+        case "Banners":
+            printy("Banners Selected", yellow)
+            
+        case "Port Scan":
+            printy("Port Scan Selected", yellow)
+        case "Exit":
+            printy("Exiting", yellow)
+            subprocess.run("clear")
+            exit()
+        case _: 
+            printy("Invalid Option", yellow)
+            menu_nmap()
